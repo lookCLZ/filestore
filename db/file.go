94853d14e000190a43fd6716a7ade61e0968c607
lookNa2CO3 <lookinward.qq@gmail.com>
2020-04-18 13:59:21 +0800

    x

--- db/file.go
+++ db/file.go
@@ -2,28 +2,29 @@ package db
 
 import (
 	mydb "filestore/db/mysql"
+	"fmt"
 )
 
 // 文件上传完成，保存meta
-func OnFileUploadFinished(filehash string,filename string,filesize int64,fileaddr string)bool{
-	stmt,err:=mydb.DBConn().Prepare(
-		"insert ignore into tbl_file (`file_sha1`,`file_name`,`file_size`"+
-		"`file_addr`,`status`) values(?,?,?,?,1)"
+func OnFileUploadFinished(filehash string, filename string, filesize int64, fileaddr string) bool {
+	stmt, err := mydb.DBConn().Prepare(
+		"insert ignore into tbl_file (`file_sha1`,`file_name`,`file_size`" +
+			"`file_addr`,`status`) values(?,?,?,?,1)",
 	)
-	if err!=nil{
-		fmt.Println("Failed to prepare statement,err:"+err.Error())
+	if err != nil {
+		fmt.Println("Failed to prepare statement,err:" + err.Error())
 		return false
 	}
 	defer stmt.Close()
 
-	ret,err:=stmt.Exec(filehash,filename,filesize,fileaddr)
-	if err!=nil{
+	ret, err := stmt.Exec(filehash, filename, filesize, fileaddr)
+	if err != nil {
 		fmt.Println(err.Error())
-		return false 
+		return false
 	}
-	if rf,err:=ret.RowsAffected();nil==err{
-		if rf < = 0{
-			fmt.Printf("File with hash:%s has been uploaded before",filehash)
+	if rf, err := ret.RowsAffected(); nil == err {
+		if rf <= 0 {
+			fmt.Printf("File with hash:%s has been uploaded before", filehash)
 		}
 		return true
 	}
