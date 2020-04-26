7b33ba11bcaf1d8c4a079ef6ad305972d018ed7c
lookNa2CO3 <lookinward.qq@gmail.com>
2020-04-26 08:59:45 +0800

    x

---
+++ handler/mpupload.go
@@ -0,0 +1,66 @@
+package handler
+
+import (
+	"filestore/util"
+	"fmt"
+	"math"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+	"time"
+)
+
+type MultipartUploadInfo struct {
+	FileHash   string
+	FileSize   int
+	UploadID   string
+	ChunkSize  int
+	ChunkCount int
+}
+
+func InitalMultipartUploadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.Form.Get("username")
+	filehash := r.Form.Get("filehash")
+	filesize, err := strconv.Atoi(r.Form.Get("filesize"))
+	if err != nil {
+		w.Write(util.NewRespMsg(-1, "params invalid", nil).JSONBytes())
+		return
+	}
+
+	rConn := rPool.RedisPool().Get()
+	defer rConn.Close()
+
+	upInfo := MultipartUploadInfo{
+		FileHash:   filehash,
+		FileSize:   filesize,
+		UploadID:   username + fmt.Sprintf("%x", time.Now().UnixNano()),
+		ChunkSize:  5 * 1024 * 1024,
+		ChunkCount: int(math.Ceil(float64(filesize) / (5 * 1024 * 1024))),
+	}
+
+	rConn.Do("HSET", "MP_"+upInfo.UploadID, "chunkcount", upInfo.ChunkCount)
+	rConn.Do("HSET", "MP_"+upInfo.UploadID, "filehash", upInfo.FileHash)
+	rConn.Do("HSET", "MP_"+upInfo.UploadID, "filesize", upInfo.FileSize)
+
+	w.Write(util.NewRespMsg(0, "OK", upInfo).JSONBytes())
+}
+
+func UploadPartHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	uploadID := r.Form.Get("uploadid")
+	chunkIndex := r.Form.Get("index")
+
+	rConn := rPool.RedisPool.Get()
+	defer rConn.Close()
+
+	fpath := "/data/" + uploadID + "/" + chunkIndex
+	os.MkdirAll(path.Dir(fpath), 0744)
+	fd, err := os.Create(fpath)
+	if err != nil {
+		w.Write(util.NewRespMsg(-1, "Upload part failed", nil), JSONBytes())
+		return
+	}
+	defer fd.Close()
+}
